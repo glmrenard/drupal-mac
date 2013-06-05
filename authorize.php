99b62ee1c3f49a777e7859146e793b24f2ed6bb4
Guillaume RENARD <guillaume.renard@arte.tv>
2013-06-05 15:25:49 +0200

    Update file entitiy module

--- authorize.php
+++ authorize.php
@@ -59,7 +59,6 @@ function authorize_access_allowed() {
 }
 
 // *** Real work of the script begins here. ***
-
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/includes/common.inc';
 require_once DRUPAL_ROOT . '/includes/file.inc';
